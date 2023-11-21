26ac36e4fc6f1bcea3c124cb40f741f04ee1a401
firas <firasazzebi66@gmail.com>
2023-11-21 03:49:17 +0100

    complete user front and back

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const DB = process.env.DB
+const DB = process.env.DB;
 
-<<<<<<<<< Temporary merge branch 1
-mongoose.connect(`mongodb+srv://firasazzebi66:${DB}@cluster0.z3mogai.mongodb.net/beatbones?retryWrites=true&w=majority`).then(()=>console.log('connected to the database')).catch((err)=>console.log(err))
-=========
-mongoose.connect(`mongodb+srv://firasazzebi66:${DB}@cluster0.z3mogai.mongodb.net/?retryWrites=true&w=majority`).then(()=>console.log('connected to the database')).catch((err)=>console.log(err))
->>>>>>>>> Temporary merge branch 2
+mongoose
+  .connect(
+    `mongodb+srv://firasazzebi66:${DB}@cluster0.z3mogai.mongodb.net/beatbones?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("connected to the database"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 
